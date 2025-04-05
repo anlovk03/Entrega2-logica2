e7d22afca7f80835129f492e9336030571a2a83a
Ana Sofía <osoriomartineza99@gmail.com>
2025-04-05 15:53:46 -0500

    Delete App.java

--- App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        Menu menu = new Menu();
-        menu.MostrarMenu();
-        
-
-    }
-}
